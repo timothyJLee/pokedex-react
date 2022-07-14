78fbb957b9ad2947cc651fca07d5a33e8ff8ae41
Timothy Lee <timothy.j.lee.cs@gmail.com>
2022-07-14 12:16:58 -0400

    Initial React

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Navbar from './Components/Navbar';
+
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navbar name="person" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,9 @@
+
+import React, {Component} from 'react';
+
+
+function Navbar(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+export default Navbar;
