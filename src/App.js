c8be99f60a6bb3a313551400af1787d281500ecb
Timothy Lee <timothy.j.lee.cs@gmail.com>
2023-08-14 10:50:08 -0400

    API Props URL

--- src/App.js
+++ src/App.js
@@ -1,61 +1,45 @@
 import { BrowserRouter as Router } from "react-router-dom";
-import Pokedex from "pokedex-promise-v2";
+//import Pokedex from "pokedex-promise-v2";
 import Navbar from "./components/Navbar/Navbar";
-import Pokemon from "./components/Pokemon";
+//import Pokemon from "./components/Pokemon";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 import "./App.css";
 
 function App(props) {
-
-
-  const [pokemon, setPokemon] = useState("null");
-  const [userInput, setUserInput] = useState("null");
-  //const [pokeAbility, setPokeAbility] = useState("null");
-  //const [data, setData] = useState(null);
+  const [pokemon, setPokemon] = useState([]);
+  //const [userInput, setUserInput] = useState("null");
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(true);
+  const [error, setError] = useState(null);
 
- // const P = new Pokedex();
- // P.getPokemonByName("pikachu") // with Promise
- // .then((response) => {
- //   console.log(response.abilities[1].ability.name);
-   // setPokeAbility(response.abilities[1].ability.name);
-    //setPokemon(response);
- // })
-//  .catch((error) => {
-//    console.log("There was an ERROR: ", error);
-//  });
+let id = 1;
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${id}`
+        );
+        setPokemon(await response.data);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setPokemon(null);
+        console.log("The error is " + error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  },[]);
 
- // P.getPokemonsList().then((response) => {
- //   console.log(response.results);
- // });
-
-useEffect(() => {
-  const getData = async () => {
-    try {
-      let id = 1;
-      const response = await axios.get(
-        `https://pokeapi.co/api/v2/pokemon/${id}`
-      );
-      setPokemon(response.data);
-      setError(null);
-    } catch (err) {
-      setError(err.message);
-      setPokemon(null);
-    } finally {
-      setLoading(false);
-    }
-  };
-  getData();
-}, []);
+  if (!loading) {
+    console.log("the id is: " + pokemon.id);
+  }
 
   return (
     <div className="App">
-      <Router>
-        <Navbar pokemon={pokemon} />
-      </Router>
+      <Router>{!loading && <Navbar pokemon={pokemon} />}</Router>
     </div>
   );
 }
--- src/Components/Pokemon.ts
+++ src/Components/Pokemon.ts
@@ -1,12 +1,38 @@
 
-import React from "react";
-import Pokedex from "pokedex-promise-v2";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 
 function Pokemon(props : any){
 
+ const [pokemon, setPokemon] = useState(null);
+ const [loading, setLoading] = useState(true);
+ const [error, setError] = useState(null);
+
+ useEffect(() => {
+   const getData = async () => {
+     try {
+       let id = 1;
+       const response = await axios.get(
+         `https://pokeapi.co/api/v2/pokemon/${id}`
+       );
+       setPokemon(response.data);
+       setError(null);
+     } catch (err) {
+      
+       //setError(err.message);
+       setPokemon(null);
+       console.log("The error is " + error);
+     } finally {
+       setLoading(false);
+     }
+   };
+   getData();
+ }, [pokemon, error, loading]);
+
 
     return {
+
   }
 }
 
